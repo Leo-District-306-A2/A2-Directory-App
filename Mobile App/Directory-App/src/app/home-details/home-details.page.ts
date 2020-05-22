199a9fa9eaafa6a57f0eeb7a69b5c8c81692bce5
pasinduSudesh <sudeshdilshan.17@cse.mrt.ac.lk>
2020-05-22 21:04:48 +0530

    created home tab

---
+++ Mobile App/Directory-App/src/app/home-details/home-details.page.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+
+@Component({
+  selector: 'app-home-details',
+  templateUrl: './home-details.page.html',
+  styleUrls: ['./home-details.page.scss'],
+})
+export class HomeDetailsPage implements OnInit {
+
+  viewData:any
+
+  constructor(private route: ActivatedRoute) {
+    route.paramMap.subscribe((data) => {
+      this.viewData = JSON.parse(data.get('data'));
+    });
+   }
+
+  ngOnInit() {
+  }
+
+}
--- Mobile App/Directory-App/src/app/home/home.page.ts
+++ Mobile App/Directory-App/src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HomeService } from '../home.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +9,53 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  nationalAnthemEnglish:any
+  nationalAnthemSinhala:any
+  pledgeOfAllegianceEnglish:any
+  pledgeOfAllegianceSinhala:any
+  leoPledge:any
+  leoHistory:any
+  parisaraPrathignawa:any
+ 
+
+  constructor(private homeService: HomeService, private router: Router) {}
+
+  ngOnInit(){
+    this.homeService.getNationalAnthemEnglish().then(result=>this.nationalAnthemEnglish = result)
+    this.homeService.getNationalAnthemSinhala().then(result=>this.nationalAnthemSinhala = result)
+    this.homeService.getPledgeOfAllegianceEnglish().then(result=>this.pledgeOfAllegianceEnglish = result)
+    this.homeService.getPledgeOfAllegianceSinhala().then(result=>this.pledgeOfAllegianceSinhala = result)
+    this.homeService.getLeoPledge().then(result=>this.leoPledge = result)
+    this.homeService.getLeoHistory().then(result=>this.leoHistory = result)
+    this.homeService.getParisaraPrathignawa().then(result=>this.parisaraPrathignawa = result)    
+  }
+
+  fullView(type){
+    switch (type) {
+      case 'NATIONAL_ANTHEM_SINHALA':
+        this.router.navigate(['./home-details', {data: JSON.stringify(this.nationalAnthemSinhala)}]);
+        break;
+      case 'NATIONAL_ANTHEM_ENGLISH':
+        this.router.navigate(['./home-details', {data: JSON.stringify(this.nationalAnthemEnglish)}]);
+        break;
+      case 'LEO_PLEDGE_OF_ALLEGIANCE_SINHALA':
+        this.router.navigate(['./home-details', {data: JSON.stringify(this.pledgeOfAllegianceSinhala)}]);
+        break;
+      case 'LEO_PLEDGE_OF_ALLEGIANCE_ENGLISH':
+        this.router.navigate(['./home-details', {data: JSON.stringify(this.pledgeOfAllegianceEnglish)}]);
+        break;
+      case 'LEO_PLEDGE':
+        this.router.navigate(['./home-details', {data: JSON.stringify(this.leoPledge)}]);
+        break;
+      case 'LEO_HISTORY':
+        this.router.navigate(['./home-details', {data: JSON.stringify(this.leoHistory)}]);
+        break;
+      case 'PARISARA_PRATHIGNAWA':
+        this.router.navigate(['./home-details', {data: JSON.stringify(this.parisaraPrathignawa)}]);
+        break;  
+      default:
+        break;
+    }
+  }
 
 }
