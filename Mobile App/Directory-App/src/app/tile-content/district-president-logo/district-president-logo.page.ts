f19e7a2ba12f8de38fb9aba6fb25c880e087d015
ThilinaPrasad <thilinaprasad.15@cse.mrt.ac.lk>
2020-07-18 11:54:42 +0530

    fixed refactoring issues

--- Mobile App/Directory-App/src/app/tile-content/district-president-logo/district-president-logo.page.ts
+++ Mobile App/Directory-App/src/app/tile-content/district-president-logo/district-president-logo.page.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {UtilityService} from '../../services/utility.service';
+import {Env} from '../../services/env';
 
 @Component({
     selector: 'app-district-president-logo',
@@ -14,10 +15,10 @@ export class DistrictPresidentLogoPage implements OnInit {
     logoUrl: string;
 
     constructor(private route: ActivatedRoute,
-                public utilityService: UtilityService) {
+                public utilityService: UtilityService, public env: Env) {
         route.paramMap.subscribe((data) => {
             this.viewData = JSON.parse(data.get('data'));
-            this.logoUrl = this.viewData.logo;
+            this.logoUrl = this.viewData.image;
             console.log(this.viewData);
         });
     }
@@ -28,10 +29,10 @@ export class DistrictPresidentLogoPage implements OnInit {
     toggleCollapsing(id, property: any) {
         if (this.collapseditemId === id) {
             this.collapseditemId = 0;
-            this.logoUrl = this.viewData.logo;
+            this.logoUrl = this.viewData.image;
         } else {
             this.collapseditemId = id;
-            this.logoUrl = property.logo;
+            this.logoUrl = property.image;
         }
     }
 
