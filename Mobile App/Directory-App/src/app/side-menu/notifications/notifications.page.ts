4bc66c5f1f148f7bf91f4b944fc0e47766dc16a1
ThilinaPrasad <thilinaprasad.15@cse.mrt.ac.lk>
2020-12-19 01:06:00 +0530

    after merge

--- Mobile App/Directory-App/src/app/side-menu/notifications/notifications.page.ts
+++ Mobile App/Directory-App/src/app/side-menu/notifications/notifications.page.ts
@@ -1,52 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {Env} from '../../services/env';
 import {UtilityService} from '../../services/utility.service';
 import {NotificationService} from '../../services/notification.service';
-import { Network } from '@ionic-native/network/ngx';
 import {NetworkService} from '../../services/network.service';
 
 @Component({
-  selector: 'app-notifications',
-  templateUrl: './notifications.page.html',
-  styleUrls: ['./notifications.page.scss'],
+    selector: 'app-notifications',
+    templateUrl: './notifications.page.html',
+    styleUrls: ['./notifications.page.scss'],
 })
 export class NotificationsPage implements OnInit {
-  backRoute = '/tabs/home';
-  collapseditemId = 0;
-  constructor(private router: Router,
-              private route: ActivatedRoute,
-              public env: Env,
-              public utilityService: UtilityService,
-              public notificationService: NotificationService,
-              public networkService: NetworkService) {
-    route.paramMap.subscribe((data) => {
-      if (data) {
-        this.backRoute = JSON.parse(data.get('data'));
-      } else {
-        this.backRoute = '/tabs/home';
-      }
-    });
+    backRoute = '/tabs/home';
+    collapsibleItemId = 0;
 
-    
-
-  }
-
-  ngOnInit() {
-  }
-
-  goBack() {
-    this.router.navigate([this.backRoute]).catch(() => {
-      this.router.navigate(['/tabs/home']);
-    });
-  }
-
-  toggleCollapsing(id){
-    if(this.collapseditemId === id){
-      this.collapseditemId = 0;
-    }else{
-      this.collapseditemId = id;
+    constructor(private router: Router,
+                private route: ActivatedRoute,
+                public env: Env,
+                public utilityService: UtilityService,
+                public notificationService: NotificationService,
+                public networkService: NetworkService) {
+        route.paramMap.subscribe((data) => {
+            if (data) {
+                this.backRoute = JSON.parse(data.get('data'));
+            } else {
+                this.backRoute = '/tabs/home';
+            }
+        });
     }
-  }
+
+    ngOnInit() {
+    }
+
+    goBack() {
+        if (this.backRoute) {
+            this.router.navigate([this.backRoute]);
+        } else {
+            this.router.navigate(['/tabs/home']);
+        }
+    }
+
+    toggleCollapsing(id) {
+        if (this.collapsibleItemId === id) {
+            this.collapsibleItemId = 0;
+        } else {
+            this.collapsibleItemId = id;
+        }
+    }
 
 }
